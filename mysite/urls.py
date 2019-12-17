68826bf9ce9ad223580efdb712e02bb3048132e2
Julie <julie00lee@gmail.com>
2019-12-16 22:12:32 -0500

    Changed logout view

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views
-from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LoginView.as_view(), name='logout',kwargs={'next_page': '/'}),
+    path('', include('blog.urls')),
 ]
